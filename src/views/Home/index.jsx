3412258da1aa8cb98283552d9ff66efc6b762e84
Maol <maulana.zulkifar@oppo-aed.id>
2024-06-11 09:56:35 +0700

    add compresser before upload

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
     Backdrop,
     Box,
@@ -12,10 +13,9 @@ import {
     Typography
 } from "@mui/material";
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import {useEffect, useState} from "react";
-import getImage from "../../services/ImageServices/Get.js";
-import PostImage from "../../services/ImageServices/Post.js";
-import {VisibilityOff} from "@mui/icons-material";
+import imageCompression from 'browser-image-compression';
+import getImage from "../../services/ImageServices/Get";
+import PostImage from "../../services/ImageServices/Post";
 
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -29,127 +29,130 @@ const VisuallyHiddenInput = styled('input')({
     width: 1,
 });
 
-function SearchIcon() {
-    return null;
-}
-
 const Home = () => {
     const [images, setImages] = useState([]);
     const [isLoad, setIsLoad] = useState(false);
     const [password, setPassword] = useState();
-    useEffect(() =>{
+
+    useEffect(() => {
         fetchImage();
-    },[])
+    }, []);
+
     const fetchImage = () => {
         setIsLoad(true);
         getImage().then(res => {
             setImages(res.objects);
-        }).finally(() =>{
+        }).finally(() => {
             setIsLoad(false);
-        })
-    }
-    const handlePostImage = (data) => {
+        });
+    };
+
+    const handlePostImage = async (data) => {
         if (password === 'imi2024') {
-            setIsLoad(true);
-            const formData = new FormData();
-            formData.append("image", data);
-            PostImage(formData).then(res => {
-                console.log(res);
-            }).finally(() => {
-                fetchImage();
-            })
+            const options = {
+                maxSizeMB: 1,
+                maxWidthOrHeight: 800,
+                useWebWorker: true
+            };
+
+            try {
+                setIsLoad(true);
+                const compressedFile = await imageCompression(data, options);
+                const formData = new FormData();
+                formData.append("image", compressedFile);
+                PostImage(formData).then(res => {
+                    console.log(res);
+                }).finally(() => {
+                    fetchImage();
+                });
+            } catch (error) {
+                console.error("Error while compressing image", error);
+            } finally {
+                setIsLoad(false);
+            }
         }
-    }
-    return(
-      <Container>
-          <Backdrop
-              sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-              open={password !== 'imi2024'}
-          >
-              <Dialog
-                  open={password !== 'imi2024'}
-              >
-                  <DialogTitle>Locked</DialogTitle>
-                  <DialogContent>
-                      <DialogContentText>
-                          Masukan Password untuk mengakses halaman atau minta ke administrator
-                      </DialogContentText>
-                      <TextField
-                          autoFocus
-                          required
-                          margin="dense"
-                          id="password"
-                          name="password"
-                          label="Password"
-                          type="password"
-                          fullWidth
-                          variant="outlined"
-                          onChange={event => setPassword(event.target.value)}
-                      />
-                  </DialogContent>
-              </Dialog>
-          </Backdrop>
-          {
-              isLoad? <Backdrop
-                  sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                  open={true}
-              >
-                  <CircularProgress color="inherit" />
-              </Backdrop>:''
-          }
+    };
 
-          <Typography variant={'h4'}>Media<br/>Image Hosting Server</Typography>
-
-          <Box paddingY={2}>
-              <Button
-                  component="label"
-                  role={undefined}
-                  variant="contained"
-                  tabIndex={-1}
-                  startIcon={<CloudUploadIcon />}
-              >
-                  Upload file
-                  <VisuallyHiddenInput type="file" onInput={e => handlePostImage(e.target.files[0])}/>
-              </Button>
-          </Box>
-
-          <Box sx={{display: 'flex'}} flexWrap={'wrap'} justifyContent={"center"} columnGap={2} rowGap={2}>
-              {
-                  images.toReversed().map(item => {
-                      return(
-                          <div key={item.key}>
-                              <Card sx={{ width: 345 }}>
-                                      <CardMedia
-                                          component="img"
-                                          height="140"
-                                          image={'https://image-hosting.maulanazulkifar.com/'+item.key}
-                                          alt="green iguana"
-                                      />
-                                      <CardContent>
-                                          <Box sx={{display: 'flex'}}>
-                                              <input
-                                                  readOnly
-                                                  className='form-control'
-                                                  type='text'
-                                                  id='input-with-icon-adornment'
-                                                  placeholder='Search order'
-                                                  aria-label='Search'
-                                                  value={'https://image-hosting.maulanazulkifar.com/'+item.key}
-                                              />
-                                              <Button onClick={() => navigator.clipboard.writeText('https://image-hosting.maulanazulkifar.com/'+item.key)}>
-                                                  Copy
-                                              </Button>
-                                          </Box>
-                                      </CardContent>
-                              </Card>
-                          </div>
-                      )
-                  })
-              }
-          </Box>
-
-      </Container>
-    )
-}
+    return (
+        <Container>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={password !== 'imi2024'}
+            >
+                <Dialog open={password !== 'imi2024'}>
+                    <DialogTitle>Locked</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Masukan Password untuk mengakses halaman atau minta ke administrator
+                        </DialogContentText>
+                        <TextField
+                            autoFocus
+                            required
+                            margin="dense"
+                            id="password"
+                            name="password"
+                            label="Password"
+                            type="password"
+                            fullWidth
+                            variant="outlined"
+                            onChange={event => setPassword(event.target.value)}
+                        />
+                    </DialogContent>
+                </Dialog>
+            </Backdrop>
+            {isLoad && (
+                <Backdrop
+                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                    open={true}
+                >
+                    <CircularProgress color="inherit" />
+                </Backdrop>
+            )}
+            <Typography variant={'h4'}>Media<br/>Image Hosting Server</Typography>
+            <Box paddingY={2}>
+                <Button
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    startIcon={<CloudUploadIcon />}
+                >
+                    Upload file
+                    <VisuallyHiddenInput type="file" onInput={e => handlePostImage(e.target.files[0])} />
+                </Button>
+            </Box>
+            <Box sx={{display: 'flex'}} flexWrap={'wrap'} justifyContent={"center"} columnGap={2} rowGap={2}>
+                {images.toReversed().map(item => (
+                    <div key={item.key}>
+                        <Card sx={{ width: 345 }}>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={'https://image-hosting.maulanazulkifar.com/' + item.key}
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Box sx={{display: 'flex'}}>
+                                    <input
+                                        readOnly
+                                        className='form-control'
+                                        type='text'
+                                        id='input-with-icon-adornment'
+                                        placeholder='Search order'
+                                        aria-label='Search'
+                                        value={'https://image-hosting.maulanazulkifar.com/' + item.key}
+                                    />
+                                    <Button onClick={() => navigator.clipboard.writeText('https://image-hosting.maulanazulkifar.com/' + item.key)}>
+                                        Copy
+                                    </Button>
+                                </Box>
+                            </CardContent>
+                        </Card>
+                    </div>
+                ))}
+            </Box>
+        </Container>
+    );
+};
 
 export default Home;
