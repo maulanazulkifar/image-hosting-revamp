2e93103a4e7c67d292c589733571d1e63d1e06e4
Maol <maulana.zulkifar@oppo-aed.id>
2024-12-16 11:37:19 +0700

    reverse array

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://47823382de192432be4cee51a7cd9b42@o4507405751877632.ingest.us.sentry.io/4507405756006400",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
